9fce5d2089634f51a69f548760a0c10f4cf39579
FrisoDev <frisodevstoker@gmail.com>
2018-03-16 15:28:16 +0100

    Student Ditch

--- studentselector/selector.js
+++
@@ -1,9 +0,0 @@
-const algo = require('./algo')
-
-const studentSelector = (students) => {
-
-  const students = algo(students)
-  return students[Math.floor(Math.random()*students.length)];
-
-}
-module.exports = studentSelector
